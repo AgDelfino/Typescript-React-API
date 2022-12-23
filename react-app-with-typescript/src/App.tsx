660c4b2e53d2a3c255d1d55a4aca5df1c2036c8c
AgDelfino <agustin.delfino92@gmail.com>
2022-12-23 19:38:09 -0300

    First Commit - React App Created

---
+++ react-app-with-typescript/src/App.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+
+import './App.css';
+
+function App() {
+
+  const [number, setNumber ] = useState(5); 
+
+  const changeNumber = () => {
+    setNumber("2")
+  }
+
+  return (
+    <div className="App">
+      {number}
+      <button onClick={changeNumber}></button>
+    </div>
+  );
+}
+
+export default App;
